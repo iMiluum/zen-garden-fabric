42d133c4dbab59baf3c7af55868e630a18916aa5
Miluum <imiluum@gmail.com>
2023-11-16 15:48:56 +0100

    Initial commit

---
+++ src/client/java/com/miluum/zen_garden/Zen_gardenClient.java
@@ -0,0 +1,10 @@
+package com.miluum.zen_garden;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Zen_gardenClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
